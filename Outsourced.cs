7eb94c862206067da67dcd57a979c42fdfb6e03a
Frank-Develops <167141334+Frank-Develops@users.noreply.github.com>
2024-06-22 19:12:03 -0500

    some classes

---
+++ Outsourced.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventorySystem_Frank_Bishop
+{
+    internal class Outsourced : Part
+    {
+        public String CompanyName;
+    }
+}
